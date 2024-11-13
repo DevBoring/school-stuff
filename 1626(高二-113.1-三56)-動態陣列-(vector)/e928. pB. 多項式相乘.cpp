ac6db473ee957af5214cef9015ac7277fd117ea4
DevBoring <104751875+DevBoring@users.noreply.github.com>
2024-11-13 23:44:40 +0800

    Create e928. pB. 多項式相乘.cpp

---
+++ 1626(高二-113.1-三56)-動態陣列-(vector)/e928. pB. 多項式相乘.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v1;
+    vector<int> v2;
+    int n1, n2, n;
+    cin>>n1;
+    for(int i = 0; i < n1+1; i++)
+    {
+        cin>>n;
+        v1.push_back(n);
+    }
+    cin>>n2;
+    for(int i = 0; i < n2+1; i++)
+    {
+        cin>>n;
+        v2.push_back(n);
+    }
+    cout<<n1+n2<<"\n";
+    int v3[n1+n2+1] = {};
+    for(int i = 0; i <= n1+n2; i++)
+    {
+        for(int j = 0; j<=n1; j++)
+        {
+            for(int k = 0; k<=n2; k++)
+            {
+                if(j+k == i)
+                {
+                    v3[i] = v3[i] + v1[j] * v2[k];
+                }
+            }
+        }
+    }
+    for (auto e:v3)
+    {
+        cout<<e<<" ";
+    }
+}
