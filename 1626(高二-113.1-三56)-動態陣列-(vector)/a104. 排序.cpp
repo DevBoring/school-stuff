9eb51b7d48471e5493fa3821f6ba2407d1a4a262
DevBoring <104751875+DevBoring@users.noreply.github.com>
2024-12-04 14:01:02 +0800

    Create a104. 排序.cpp

---
+++ 1626(高二-113.1-三56)-動態陣列-(vector)/a104. 排序.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+
+    int n,n1;
+    while(cin>>n)
+    {
+        vector<int> v;
+        for (int i = 0; i < n; i++)
+        {
+            cin>>n1;
+            v.push_back(n1);
+        }
+        sort(v.begin(), v.end());
+        for (auto e:v)
+        {
+            cout<<e<<" ";
+        }
+        cout<<"\n";
+    }
+
+
+
+
+}
